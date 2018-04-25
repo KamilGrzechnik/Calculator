6c45c38da08e0643b8b58cc798c2861d4b4df842
Kamil <Kamil@LAPTOP-1JOTA28K>
2018-04-25 10:45:09 +0200

    connecting with assembler

--- Calculator/Methods/AsmProxy.cs
+++ Calculator/Methods/AsmProxy.cs
@@ -1,15 +1,21 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Calculator.Methods
 {
     public class AsmProxy
     {
         [DllImport("Asm.dll")]
-        private static extern float AsmAddVec(float a1, float a2, float a3, float a4, float b1, float b2, float b3, float b4);
+        public static extern float[] AsmAddition(float[] table1, float[] table2);
 
-        public double ExecuteAsmAddVec(float a1, float a2, float a3, float a4, float b1, float b2, float b3, float b4)
-        {
-            return AsmAddVec(a1, a2, a3, a4, b1, b2, b3, b4);
-        }
+        [DllImport("Asm.dll")]
+        public static extern float[] AsmSubstraction(float[] table1, float[] table2);
+
+        [DllImport("Asm.dll")]
+        public static extern float[] AsmMultiplication(float[] table1, float[] table2);
+
+        [DllImport("Asm.dll")]
+        public static extern float[] AsmDivision(float[] table1, float[] table2);
+
     }
 }
--- Calculator/Methods/AssemblerHandler.cs
+++ Calculator/Methods/AssemblerHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace Calculator.Methods
 {
@@ -14,26 +15,43 @@ namespace Calculator.Methods
 
         public List<float> Addition(List<float> floatList)
         {
+            var table = floatList.ToArray();
+            var table1 = floatList.GetRange(0, 3).ToArray();
+            var table2 = floatList.GetRange(4, 7).ToArray();
+            table1 = AsmProxy.AsmAddition(table1, table2);
             var list = new List<float>() { 1, 1, 1, 1 };
             return list;
+            //return new List<float>(table1);
         }
 
         public List<float> Substraction(List<float> floatList)
         {
+            var table1 = floatList.GetRange(0,3).ToArray();
+            var table2 = floatList.GetRange(4, 7).ToArray();
+            table1 = AsmProxy.AsmSubstraction(table1,table2);
             var list = new List<float>() { 2, 2, 2, 2 };
             return list;
+            //return new List<float>(table1);
         }
 
         public List<float> Multiplication(List<float> floatList)
         {
+            var table1 = floatList.GetRange(0, 3).ToArray();
+            var table2 = floatList.GetRange(4, 7).ToArray();
+            table1 = AsmProxy.AsmMultiplication(table1, table2);
             var list = new List<float>() { 3, 3, 3, 3 };
             return list;
+            //return new List<float>(table1);
         }
 
         public List<float> Division(List<float> floatList)
         {
+            var table1 = floatList.GetRange(0, 3).ToArray();
+            var table2 = floatList.GetRange(4, 7).ToArray();
+            table1 = AsmProxy.AsmDivision(table1, table2);
             var list = new List<float>() { 4, 4, 4, 4 };
             return list;
+            //return new List<float>(table1);
         }
 
     }
